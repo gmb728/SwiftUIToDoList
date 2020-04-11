05b76941beba393c7c8e5062a6586c938a7961be
Sophia <gmb728@gmail.com>
2020-04-11 20:49:38 +0800

    Initial Commit

---
+++ SwiftUIToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIToDoList
+//
+//  Created by Chang Sophia on 4/11/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
